bd078480b29561539ff988ad37d4458779e0e241
Daryl Alex <88949627+drlalx007@users.noreply.github.com>
2023-06-27 13:04:13 -0400

    Add files via upload

---
+++ w05/w5p2.c
@@ -0,0 +1,333 @@
+﻿/*
+	==================================================
+	Workshop #5 (Part-2):
+	==================================================
+	Name   : Daryl Alex
+	ID     : 143691202
+	Email  : dalex4@myseneca.ca
+	Section: ZBB
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#define Maximum_life 10
+#define Maximum_path_length 70
+#define Minimum_path_length 10
+#define Multiple 5
+typedef struct Playerinfo {
+	int number_of_lives;
+	char symbol;
+	int number_of_tresure;
+	int enter_position[Maximum_path_length];
+	char history[Maximum_path_length];
+}player_info;
+typedef struct GameInfo {
+	int maximum_moves;
+	int path_length;
+	int bombs[Maximum_path_length];
+	int tressure[Maximum_path_length];
+}game_info;
+int main()
+{
+	player_info pi;
+	game_info gi;
+	int i;
+	printf("================================\n");
+	printf("         Treasure Hunt!         \n");
+	printf("================================\n\n");
+	printf("PLAYER Configuration\n");
+	printf("--------------------\n");
+	printf("Enter a single character to represent the player: ");
+	scanf("%c", &pi.symbol);
+	do
+	{
+		printf("Set the number of lives: ");
+		scanf("%d", &pi.number_of_lives);
+		if (pi.number_of_lives < 1 || pi.number_of_lives > Maximum_life)
+		{
+			printf("     Must be between 1 and 10!\n");
+		}
+	} while (pi.number_of_lives < 1 || pi.number_of_lives > Maximum_life);
+	printf("Player configuration set-up is complete\n\n");
+	printf("GAME Configuration\n");
+	printf("------------------\n");
+	do
+	{
+		printf("Set the path length (a multiple of 5 between 10-70): ");
+		scanf("%d", &gi.path_length);
+		if (gi.path_length < Minimum_path_length || gi.path_length > Maximum_path_length || gi.path_length % Multiple != 0)
+		{
+			printf("     Must be a multiple of %d and between %d-%d!!!\n", Multiple, Minimum_path_length, Maximum_path_length);
+		}
+	} while (gi.path_length < Minimum_path_length || gi.path_length > Maximum_path_length || gi.path_length % Multiple != 0);
+	int Maximum_path_plays = (int)(0.75 * gi.path_length);
+	do
+	{
+		printf("Set the limit for number of moves allowed: ");
+		scanf("%d", &gi.maximum_moves);
+		if (gi.maximum_moves < pi.number_of_lives || gi.maximum_moves>Maximum_path_plays)
+		{
+			printf("    Value must be between %d and %d\n", pi.number_of_lives, Maximum_path_plays);
+		}
+	} while (gi.maximum_moves < pi.number_of_lives || gi.maximum_moves>Maximum_path_plays);
+	printf("\nBOMB Placement\n");
+	printf("--------------\n");
+	printf("Enter the bomb positions in sets of 5 where a value\n");
+	printf("of 1=BOMB, and 0=NO BOMB. Space-delimit your input.\n");
+	printf("(Example: 1 0 0 1 1) NOTE: there are %d to set!\n", gi.path_length);
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		if ((i - 1) % Multiple == 0)
+		{
+			printf("   Positions [%2d-%2d]: ", (i), (i + 4));
+		}
+		scanf("%d", &gi.bombs[i]);
+	}
+	printf("BOMB placement set\n\n");
+	printf("TREASURE Placement\n");
+	printf("------------------\n");
+	printf("Enter the treasure placements in sets of 5 where a value\n");
+	printf("of 1=TREASURE, and 0=NO TREASURE. Space-delimit your input.\n");
+	printf("(Example: 1 0 0 1 1) NOTE: there are %d to set!\n", gi.path_length);
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		if ((i - 1) % Multiple == 0)
+		{
+			printf("   Positions [%2d-%2d]: ", (i), (i + 4));
+		}
+		scanf("%d", &gi.tressure[i]);
+	}
+	printf("TREASURE placement set\n\n");
+	printf("GAME configuration set-up is complete...\n\n");
+	printf("------------------------------------\n");
+	printf("TREASURE HUNT Configuration Settings\n");
+	printf("------------------------------------\n");
+	printf("Player:\n");
+	printf("   Symbol     : %c\n", pi.symbol);
+	printf("   Lives      : %d\n", pi.number_of_lives);
+	printf("   Treasure   : [ready for gameplay]\n");
+	printf("   History    : [ready for gameplay]\n\n");
+	printf("Game:\n");
+	printf("   Path Length: %d\n", gi.path_length);
+	printf("   Bombs      : ");
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		printf("%d", gi.bombs[i]);
+	}
+	printf("\n   Treasure   : ");
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		printf("%d", gi.tressure[i]);
+	}
+	printf("\n\n");
+	printf("====================================\n");
+	printf("~ Get ready to play TREASURE HUNT! ~\n");
+	printf("====================================\n\n");
+	int count = 0;
+	pi.number_of_tresure = 0;
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		pi.enter_position[i] = 0;
+		pi.history[i] = '-';
+	}
+	do
+	{
+	forward:
+		printf("  ");
+		for (i = 1; i <= gi.path_length; i++)
+		{
+			if (pi.enter_position[count] == i && pi.enter_position[count] != 0)
+			{
+				printf("%c", pi.symbol);
+			}
+			else if (pi.enter_position[count] == 0)
+			{
+				goto c;
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+		printf("  ");
+	c:
+		for (i = 1; i <= gi.path_length; i++)
+		{
+
+			if (gi.tressure[count] == 1 && gi.bombs[count] == 1)
+			{
+				pi.history[count] = '&';
+			}
+			else if (gi.tressure[count] == 1)
+			{
+				pi.history[count] = '$';
+			}
+			else if (gi.bombs[count] == 1)
+			{
+				pi.history[count] = '!';
+			}
+			else
+			{
+				pi.history[count] = '.';
+			}
+		}
+		for (i = 1; i <= gi.path_length; i++)
+		{
+			printf("%c", pi.history[i]);
+		}
+		printf("\n");
+		printf("  ");
+		count = 0;
+		for (i = 1; i <= gi.path_length; i++)
+		{
+			if (i % 10 == 0)
+			{
+				count++;
+				printf("%d", count);
+			}
+			else
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+		printf("  ");
+		count = 1;
+		for (i = 1; i <= gi.path_length; i++)
+		{
+			printf("%d", count);
+			count += 1;
+			if (count > 9)
+			{
+				count = 0;
+			}
+		}
+		printf("\n");
+		printf("+---------------------------------------------------+\n");
+		printf("  Lives: %2d  | Treasures: %2d  |  Moves Remaining: %2d\n", pi.number_of_lives, pi.number_of_tresure, gi.maximum_moves);
+		printf("+---------------------------------------------------+\n");
+		do
+		{
+			printf("Next Move [1-%2d]: ", gi.path_length);
+			scanf("%d", &count);
+			if (count<1 || count>gi.path_length)
+			{
+				printf("  Out of Range!!!\n");
+			}
+		} while (count<1 || count>gi.path_length);
+		if (pi.enter_position[count] != 0)
+		{
+			printf("\n===============> Dope! You've been here before!\n\n");
+			goto forward;
+		}
+		pi.enter_position[count] = count;
+		if (gi.tressure[count] == 1 && gi.bombs[count] == 1)
+		{
+			if (gi.tressure[count] == gi.bombs[count])
+			{
+				printf("\n===============> [&] !!! BOOOOOM !!! [&]\n");
+				printf("===============> [&] $$$ Life Insurance Payout!!! [&]\n\n");
+				pi.number_of_lives -= 1;
+				pi.number_of_tresure += 1;
+				goto l;
+			}
+		}
+		else if (gi.bombs[count] == 1)
+		{
+			printf("\n===============> [!] !!! BOOOOOM !!! [!]\n\n");
+			pi.number_of_lives -= 1;
+		}
+		else if (gi.tressure[count] == 1)
+		{
+			printf("\n===============> [$] $$$ Found Treasure! $$$ [$]\n\n");
+			pi.number_of_tresure += 1;
+		}
+		else
+		{
+			printf("\n===============> [.] ...Nothing found here... [.]\n\n");
+		}
+	l:
+		gi.maximum_moves -= 1;
+		if (pi.number_of_lives == 0)
+		{
+			printf("No more LIVES remaining!\n\n");
+			goto i;
+		}
+	} while (pi.number_of_lives != 0 || gi.maximum_moves != 0);
+i:
+	printf("  ");
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		if (pi.enter_position[count] == i && pi.enter_position[count] != 0)
+		{
+			printf("%c", pi.symbol);
+		}
+		else
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	printf("  ");
+	for (i = 1; i <= gi.path_length; i++)
+	{
+
+		if (gi.tressure[count] == 1 && gi.bombs[count] == 1)
+		{
+			pi.history[count] = '&';
+		}
+		else if (gi.tressure[count] == 1)
+		{
+			pi.history[count] = '$';
+		}
+		else if (gi.bombs[count] == 1)
+		{
+			pi.history[count] = '!';
+		}
+		else
+		{
+			pi.history[count] = '.';
+		}
+	}
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		printf("%c", pi.history[i]);
+	}
+	printf("\n");
+	printf("  ");
+	count = 0;
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		if (i % 10 == 0)
+		{
+			count++;
+			printf("%d", count);
+		}
+		else
+		{
+			printf("|");
+		}
+	}
+	printf("\n");
+	printf("  ");
+	count = 1;
+	for (i = 1; i <= gi.path_length; i++)
+	{
+		printf("%d", count);
+		count += 1;
+		if (count > 9)
+		{
+			count = 0;
+		}
+	}
+	printf("\n");
+	printf("+---------------------------------------------------+\n");
+	printf("  Lives: %2d  | Treasures: %2d  |  Moves Remaining: %2d\n", pi.number_of_lives, pi.number_of_tresure, gi.maximum_moves);
+	printf("+---------------------------------------------------+\n\n");
+	printf("##################\n");
+	printf("#   Game over!   #\n");
+	printf("##################\n\n");
+	printf("You should play again and try to beat your score!\n");
+	return 0;
+}
