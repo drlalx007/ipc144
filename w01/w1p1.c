331119d583721de3e872094b7015efee6aec8080
Daryl Alex <88949627+drlalx007@users.noreply.github.com>
2023-06-27 13:03:03 -0400

    Add files via upload

---
+++ w01/w1p1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("*** Welcome to Seneca's C Programming Course ***\n");
+	return 0;
+
+
+}
---
+++ w01/w1p2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ Name: Daryl Alex
+ Student Number: 143691202
+ Seneca Email Address: dalex4@myseneca.ca
+ Section Information Code: ZBB
+*/
+int main()
+{
+	printf("+-------------------------------+\n");
+	printf("| Introduction to C Programming |\n");
+	printf("+-------------------------------+\n");
+	printf("Program written by:\nDaryl Alex\n");
+	return 0;
+
+
+}
